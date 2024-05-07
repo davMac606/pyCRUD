64b07d03486749c0873006f8c0a8e63b604f496b
davMac606 <cl201238@g.unicamp.br>
2024-05-07 10:08:20 -0300

    idk why i had double methods, but ok.

--- menumethods.py
+++ menumethods.py
@@ -1,4 +1,5 @@
 import os, time
+from main import menu
 
 def cls():
     os.system('cls' if os.name=='nt' else 'clear')
@@ -44,6 +45,18 @@ def pos(nom,agd):
             
     return [False,inicio]
 
+def text(inputText, mensagem, validate):
+    right_input = False
+    while not right_input:
+        txt = input(inputText)
+
+        if txt not in validate:
+            print(mensagem,'- Favor redigitar...')
+        else:
+            right_input=True
+
+    return txt
+
 def incluir(agd):
     try:
         time.sleep(1)
@@ -78,132 +91,7 @@ def incluir(agd):
     except ValueError:
         print('Erro ao incluir contato. Tente novamente.')
         incluir(agd)
-    
-def procurar(agd):
-    cls()
-    right_input = False
-    while not right_input:
-        nome = input("Digite o nome completo do contato: ")
-        Lista_decontato = pos(nome, agd)
-        achou = Lista_decontato[0]
-        posicao = Lista_decontato[1]
 
-        if not achou:
-            print("Este contato não existe, tente novamente")
-        else:
-            print('Aniversario:', agd[posicao][1])
-            print('Endereco:', agd[posicao][2])
-            print('Telefone:', agd[posicao][3])
-            print('Celular:', agd[posicao][4])
-            print('e-mail:', agd[posicao][5])
-            right_input = True
-
-    return Lista_decontato
-
-def excluir(agd):
-    cls()
-    print()
-    
-    right_input = False
-    while not right_input:
-        nome = input('NomeS: ')
-        
-        resposta = pos(nome ,agd)
-        achou = resposta[0]
-        posicao = resposta[1]
-        
-        if not achou:
-            print ('Este contato não existe. Por favor, tente novamente.')
-        else:
-            right_input = True
-    
-    print('Aniversario:',agd[posicao][1])
-    print('Endereco:',agd[posicao][2])
-    print('Telefone:',agd[posicao][3])
-    print('Celular:',agd[posicao][4])
-    print('e-mail:',agd[posicao][5])
-
-    resposta = text('Deseja realmente excluir? Insira S ou N','Você deve digitar S ou N',['s','S','n','N'])
-    
-
-def text(inputText, mensagem, validate):
-    right_input = False
-    while not right_input:
-        txt = input(inputText)
-
-        if txt not in validate:
-            print(mensagem,'- Favor redigitar...')
-        else:
-            right_input=True
-
-    return txt
-
-
-def pos(nom,agd):
-    ini  =0
-    final =len(agd)-1
-    
-    while ini<=final:
-        meio=(ini+final)//2
-        
-        if nom.upper()==agd[meio][0].upper():
-            return [True,meio]
-        elif nom.upper()<agd[meio][0].upper():
-            final=meio-1
-        else: # nom.upper()>agd[meio][0].upper()
-            ini=meio+1
-            
-    return [False,ini]
-
-def busca(agd):
-    cls()
-    nome=input('Nome a ser procurado: ')
-    resposta=pos(nome,agd)
-    achou = resposta[0]
-    posicao = resposta[1]
-    
-    if achou:
-        print('Nome:',agd[posicao][0])
-        print('Aniversário:',agd[posicao][1])
-        print('Endereço:',agd[posicao][2])
-        print('Telefone:',agd[posicao][3])
-        print('Celular:',agd[posicao][4])
-        print('e-mail:',agd[posicao][5])
-    else:
-        print('Nome não encontrado!')
-        
-def atualiza(agd):
-    cls()
-    nome=input('Nome a ser atualizado: ')
-    resposta=pos(nome,agd)
-    achou = resposta[0]
-    posicao = resposta[1]
-    
-    if achou:
-        print("O que deseja atualizar?")
-        print("[1] Nome")
-        print("[2] Aniversário")
-        print("[3] Endereço")
-        print("[4] Telefone")
-        print("[5] Celular")
-        print("[6] e-mail")
-        opcao = input("Opção: ")
-        match opcao:
-            case '1':
-                agd[posicao][0] = input("Novo nome: ")
-            case '2':
-                agd[posicao][1] = input("Novo aniversário: ")
-            case '3':
-                agd[posicao][2] = input("Novo endereço: ")
-            case '4':
-                agd[posicao][3] = input("Novo telefone: ")
-            case '5':
-                agd[posicao][4] = input("Novo celular: ")
-            case '6':
-                agd[posicao][5] = input("Novo e-mail: ")
-    else:
-        print('Nome não encontrado!')
-        
 def lista(agd):
     cls()
     print()
@@ -216,7 +104,64 @@ def lista(agd):
         print('Celular:',contato[4])
         print('e-mail:',contato[5])
         print()
-
+        
+def busca(agd):
+    cls()
+    right_input = False
+    while not right_input:
+        nome = input('Nome a ser procurado: ')
+        resposta = pos(nome, agd)
+        achou = resposta[0]
+        posicao = resposta[1]
+        if achou:
+            print('Nome:', agd[posicao][0])
+            print('Aniversário:', agd[posicao][1])
+            print('Endereço:', agd[posicao][2])
+            print('Telefone:', agd[posicao][3])
+            print('Celular:', agd[posicao][4])
+            print('e-mail:', agd[posicao][5])
+            right_input = True
+        else:
+            print('Nome não encontrado!')
+        
+def atualiza(agd):
+    cls()
+    nome=input('Nome a ser atualizado: ')
+    resposta=pos(nome,agd)
+    achou = resposta[0]
+    posicao = resposta[1]
+    
+    if achou:
+        while True:
+            print("O que deseja atualizar?")
+            print("[1] Nome")
+            print("[2] Aniversário")
+            print("[3] Endereço")
+            print("[4] Telefone")
+            print("[5] Celular")
+            print("[6] E-mail")
+            print("[7] Cancelar")
+            opcao = input("Opção: ")
+            if opcao == '1':
+                agd[posicao][0] = input("Novo nome: ")
+            elif opcao == '2':
+                agd[posicao][1] = input("Novo aniversário: ")
+            elif opcao == '3':
+                agd[posicao][2] = input("Novo endereço: ")
+            elif opcao == '4':
+                agd[posicao][3] = input("Novo telefone: ")
+            elif opcao == '5':
+                agd[posicao][4] = input("Novo celular: ")
+            elif opcao == '6':
+                agd[posicao][5] = input("Novo e-mail: ")
+            elif opcao == '7':
+                print('Atualização finalizada!')
+                break
+            else:
+                print('Opção inválida!')
+    else:
+        print('Nome não encontrado!')
+        
 def exclui(agd):
     cls()
     print()
